5a19b07ef32052254cc7fb1d073b30ab4fc92161
Gavin <xie_youjiang@163.com>
2016-12-14 21:28:53 -0800

    读写文件

---
+++ makeTextFile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import os
+ls = os.linesep
+fname = './1.txt'
+while True:
+    if os.path.exists(fname):
+    	    print "ERROR: '%s' already exists" % fname
+    else:
+    	    break
+all = []
+print "\nEnter lines('.'by itself to quit).\n"
+while True:
+    entry = raw_input('>')
+    if entry == '.':
+    	  break
+    else:
+    	  all.append(entry)
+    	  fobj = open(fname, 'w')
+    	  fobj.writelines(['%s%s' % (x, ls) for x in all])
+    	  fobj.close()
+print 'DONE!'
+	      
