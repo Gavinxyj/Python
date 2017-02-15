6a73923a386a74c039b4d9a946555fb6d897e07f
Gavin <xie_youjiang@163.com>
2017-02-15 05:01:23 -0800

    sqlalchemy

---
+++ sqlalchemyTest.py
@@ -0,0 +1,21 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column,Integer, String
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///:memory:', echo = True)
+Base = declarative_base()
+Session = sessionmaker(bind = engine)
+class User(Base):
+    __tablename__ = 'users'
+    
+    id   		 = Column(Integer, primary_key = True)
+    name 		 = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+    
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (self.name, self.fullname, self.password)
+
+print Base.metadata.create_all(engine)
